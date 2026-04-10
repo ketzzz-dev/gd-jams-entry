b20dfd2b26930b4569947a391d2b9d405ecf24a0
Keys <0hlowkey750@gmail.com>
2026-04-10 12:09:13 +0800

    main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var options_button: Button = $CenterContainer/VBoxContainer/HowToPlayButton
 @onready var quit_button: Button = $CenterContainer/VBoxContainer/QuitButton
 @onready var background: TextureRect = $Background
+@onready var center_container: CenterContainer = $CenterContainer
+@onready var how_to_panel: Control = $HowToPanel
+@onready var back_button: Button = $HowToPanel/VBoxContainer/BackButton
 
 var game: PackedScene = preload("res://scenes/mainmap.tscn")
 
@@ -11,6 +14,7 @@ func _ready() -> void:
 	start_button.pressed.connect(_on_start_pressed)
 	options_button.pressed.connect(_on_options_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
+	back_button.pressed.connect(_on_back_pressed)
 
 func _process(delta: float) -> void:
 	background.rotation_degrees += TAU * delta * 2.0
@@ -19,7 +23,12 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(game)
 
 func _on_options_pressed() -> void:
-	print("How to Play?")
+	how_to_panel.visible = true
+	center_container.visible = false
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	how_to_panel.visible = false
+	center_container.visible = true
